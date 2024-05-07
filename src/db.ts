6d5ef45336d0911703930968f10bc4d1320c60a3
Lucas Winkler <lucaswinkler@gmail.com>
2024-05-07 14:23:06 -0500

    latest packages

--- src/db.ts
+++ src/db.ts
@@ -2,9 +2,7 @@ import { neonConfig, Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
-// import dotenv from 'dotenv';
 
-// dotenv.config();
 neonConfig.webSocketConstructor = ws;
 const connectionString = process.env.DATABASE_URL;
 
